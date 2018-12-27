bcc6ea6c6374a6181e424c6a776c5a1159eb6309
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2018-12-28 00:20:31 +0100

    Move from private repository.

---
+++ flow-typed/npm/lodash.get_vx.x.x.js
@@ -0,0 +1,10 @@
+// flow-typed signature: 7d3d1522ee70343a2a57901a11843d7b
+// flow-typed version: <<STUB>>/lodash.get_v4.4.2/flow_v0.63.1
+
+declare module 'lodash.get' {
+  declare module.exports: (
+      object?: ?Object | ?Array<any>,
+      path?: ?Array<string> | string,
+      defaultValue?: any
+    ) => any;
+}
